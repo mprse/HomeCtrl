6df74e5d5081014e9ad2ee7b9d0568964b2f5b1a
bibi <johnypste@gmail.com>
2024-10-26 22:13:38 +0200

    Add support for heatting zones
    Signed-off-by: bibi <johnypste@gmail.com>

--- smart_home.c
+++ smart_home.c
@@ -15,17 +15,19 @@
 #include "dht22.h"
 
 
-#define DHT_COUNT 10
+#define DHT_COUNT 6
+#define HEAT_ZONE_COUNT 6
 #define DHT_ERR_TRESHOLD 5
-static const uint dht_pins[DHT_COUNT] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+static const uint dht_pins[DHT_COUNT] = {0, 1, 2, 4, 5, 6};
 static uint dht_err[DHT_COUNT] = {0};
 static dht_reading_t dht_dev[DHT_COUNT];
 static const uint dht_power_pin = 26;
 
+static const uint heat_zone_pins[HEAT_ZONE_COUNT] = {7, 8, 9, 10, 11, 12};
+
 #define CONT_COUNT 7
 static const uint contractron_pins[CONT_COUNT] = {16, 17, 18, 19, 20, 21, 22};
 static uint contractron_state[CONT_COUNT] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
-
 
 static const uint gate_pin = 15;
 
@@ -36,12 +38,12 @@ static bool push_dht_all = false; // push all data to client (sync)
 static bool push_con_all = false; // push all data to client (sync)
 
 static  void print_dht_error(int i, const char* error_message) {
-    printf("DHT(%d): %s\n", i, error_message);
+    printf("DHT: DHT(%d): %s\n", i, error_message);
 }
 
 static void send_dht_data_to_client(int i) {
     char msg[100];
-    sprintf(msg, "{\"dht_id\": \"%d\", \"temp\": \"%.1f\", \"hum\": \"%.1f\"}", i, dht_dev[i].temp_celsius, dht_dev[i].humidity);
+    sprintf(msg, "{\"dht_id\": \"%d\", \"temp\": \"%.1f\", \"hum\": \"%.1f\"}", dht_pins[i], dht_dev[i].temp_celsius, dht_dev[i].humidity);
     tcp_send_message(server_connection, msg);
 }
 
@@ -76,28 +78,26 @@ static void temp_task()
 
             switch (dht_result) {
                 case DHT_ERR_CHECKSUM:
-                    print_dht_error(i, "Bad checksum.");
+                    print_dht_error(dht_pins[i], "Bad checksum.");
                     dht_err[i]++;
                     break;
                 case DHT_ERR_NAN:
-                    print_dht_error(i, "Invalid value (nan).");
+                    print_dht_error(dht_pins[i], "Invalid value (nan).");
                     dht_err[i]++;
                     break;
                 case DHT_ERR_TIMEOUT:
-                    print_dht_error(i, "Sensor is not responding.");
+                    print_dht_error(dht_pins[i], "Sensor is not responding.");
                     dht_err[i]++;
                     break;
                 case DHT_OK:
                     if(dht_dev[i].temp_celsius < 10.0f || dht_dev[i].temp_celsius > 60.0f || dht_dev[i].humidity < 20.0f || dht_dev[i].humidity > 95.0f) {
-                        printf("Invalid value: DHT(%d): %.1f C, %.1f%% humidity\n", i, dht_dev[i].temp_celsius, dht_dev[i].humidity);
+                        printf("DHT: Invalid value: DHT(%d): %.1f C, %.1f%% humidity\n", i, dht_dev[i].temp_celsius, dht_dev[i].humidity);
                         dht_err[i]++;
                         break;
                     }
                     dht_err[i] = 0;
-                    printf("DHT(%d): %.1f C, %.1f%% humidity\n", i, dht_dev[i].temp_celsius, dht_dev[i].humidity);
-                    if (push_dht_all || (dht_dev[i].humidity != old.humidity || dht_dev[i].temp_celsius != old.temp_celsius)) {
-                        send_dht_data_to_client(i);
-                    }
+                    printf("DHT: DHT(%d): %.1f C, %.1f%% humidity\n", dht_pins[i], dht_dev[i].temp_celsius, dht_dev[i].humidity);
+                    send_dht_data_to_client(i);
                     break;
             }
             // 200 ms between reads
@@ -110,17 +110,10 @@ static void temp_task()
             force_sync_cnt--;
             push_dht_all = false;
         }
-        
-        for (int i = 0; i < DHT_COUNT; i++) {
-            if ((i==0 || i==1 || i==2 || i==4 || i==5 || i==6) && dht_err[i] >= DHT_ERR_TRESHOLD) {
-                printf("DHT RESET id: %d\n", i);
-                gpio_put(dht_power_pin, 0);
-                dht_err[i] = 0;
-            }
-        }
-        // Read every 1 minute
+
+        // Read every 30 sec minute
         gpio_put(dht_power_pin, 0);
-        vTaskDelay(pdMS_TO_TICKS(20000));
+        vTaskDelay(pdMS_TO_TICKS(30000));
     }
 }
 
@@ -133,7 +126,7 @@ static void contactron_task()
         for (int i = 0; i < CONT_COUNT; i++) {
             bool state = read_contactron(i);
             if (state != contractron_state[i] || push_con_all) {
-                printf("Contactron %d changed state to %d\n", i, state);
+                printf("CONT: Contactron[%d] %s\n", i, state == 0 ? "Closed" : "Open");
                 contractron_state[i] = state;
                 snprintf(msg, 50, "{\"cont_id\":\"%d\", \"val\":\"%d\"}", i, state);
                 tcp_send_message(server_connection, msg);
@@ -147,7 +140,7 @@ static void contactron_task()
 
 static void on_tcp_connection(int con_id)
 {
-    printf("TCP connected, connection id: %d\n", con_id);
+    printf("--> TCP connected, connection id: %d\n", con_id);
     cyw43_arch_gpio_put(0, true);
     server_connection = con_id;
     push_dht_all = true;
@@ -158,27 +151,41 @@ static void on_tcp_connection(int con_id)
 
 static void on_tcp_disconnection(int con_id)
 {
-    printf("TCP disconnected, connection id: %d\n", con_id);
+    printf("--> TCP disconnected, connection id: %d\n", con_id);
     cyw43_arch_gpio_put(0, false);
     server_connection = -1;
 }
 
 static void on_tcp_msg_received(int con_id, char* msg)
 {
-    printf("msg(%d): %s\n", con_id, msg);
+    char command[10];
+    int zone_id, state;
+
+    printf("> msg(%d): %s\n", con_id, msg);
 
     if (strcmp(msg, "flash") == 0) {
         reset_usb_boot(0, 0);
     } else if (strcmp(msg, "reboot") == 0) {
         reboot();
     } else if (strcmp(msg, "sync") == 0) {
-        push_dht_all = true; 
+        push_dht_all = true;
     } else if (strcmp(msg, "ping") == 0) {
         tcp_send_message(server_connection, "pong");
     } else if (strcmp(msg, "brama") == 0) {
         gpio_put(gate_pin, 1);
         sleep_ms(300);
         gpio_put(gate_pin, 0);
+    } else if (sscanf(msg, "%s %d %d", command, &zone_id, &state) == 3) {
+        // Check if the command is "heat"
+        if (strcmp(command, "heat") == 0) {
+            // Set the pin state based on the zone_id and state
+            gpio_put(heat_zone_pins[zone_id], state ? 0 : 1); // Low trigger
+            printf("HEAT: Zone[%d]: %s\n", zone_id, state == 0 ? "Disabled" : "Enabled");
+        } else {
+            printf("ERROR: Unknown command: %s\n", command);
+        }
+    } else {
+        printf("ERROR: Invalid message\n");
     }
 }
 
@@ -188,12 +195,17 @@ int main(void)
 
     printf("--- Starting HomeCtrl ---\n");
 
-    printf("Init sensors\n");
+    printf("--> Init stage\n");
 
     // Init DHT sensors
     for (int i = 0; i < DHT_COUNT; i++) {
         dht_dev[i] = dht22_init(dht_pins[i]);
     }
+
+    // Init dht power pin
+    gpio_init(dht_power_pin);
+    gpio_set_dir(dht_power_pin, GPIO_OUT);
+    gpio_put(dht_power_pin, 0);
 
     // Init contactrons
     for (int i = 0; i < CONT_COUNT; i++) {
@@ -202,16 +214,21 @@ int main(void)
         gpio_pull_up(contractron_pins[i]);
     }
 
-    gpio_init(dht_power_pin);
-    gpio_set_dir(dht_power_pin, GPIO_OUT);
-    gpio_put(dht_power_pin, 0);
+    // Init heating zones
+    for (int i = 0; i < HEAT_ZONE_COUNT; i++) {
+        gpio_init(heat_zone_pins[i]);
+        gpio_set_dir(heat_zone_pins[i], GPIO_OUT);
+        gpio_put(heat_zone_pins[i], 1);
+    }
 
+    // Init gate control pin
     gpio_init(gate_pin);
     gpio_set_dir(gate_pin, GPIO_OUT);
     gpio_put(gate_pin, 0);
 
-    printf("Wait 1 sec before connecting to wifi \n");
     sleep_ms(1000);
+
+    printf("--> Run Scheduler \n");
 
     // Init TCP server
     tcp_server_init(on_tcp_connection, on_tcp_disconnection, on_tcp_msg_received);
