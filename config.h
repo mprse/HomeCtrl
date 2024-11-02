a5c35db3741d5657403c2f6dd3858ba2dbc726a2
bibi <johnypste@gmail.com>
2024-11-02 15:08:40 +0100

    Config for up device
    Signed-off-by: bibi <johnypste@gmail.com>

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 #define ELEMENT_COUNT(array) (sizeof(array) / sizeof(array[0]))
 
 #if DHT_ENABLED == 1
-static const uint dht_pins[] = {0, 1, 2, 4, 5, 6};
+static const uint dht_pins[] = {1, 2, 3, 4, 5};
 static const uint dht_power_pin = 26;
 #define DHT_COUNT ELEMENT_COUNT(dht_pins)
 
@@ -13,7 +13,7 @@ static dht_reading_t dht_dev[DHT_COUNT];
 #endif
 
 #if HEAT_ENABLED == 1
-static const uint heat_zone_pins[] = {7, 8, 9, 10, 11, 12};
+static const uint heat_zone_pins[] = {6, 7, 8, 9, 10};
 #define HEAT_ZONE_COUNT ELEMENT_COUNT(heat_zone_pins)
 #endif
 
