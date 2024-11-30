3c601ce0b759487976ce4febbd97b14af16c0722
bibi <johnypste@gmail.com>
2024-12-01 00:25:11 +0100

    Add support for beeper
    Signed-off-by: bibi <johnypste@gmail.com>

--- smart_home.c
+++ smart_home.c
@@ -129,6 +129,111 @@ static void contactron_task()
 }
 #endif
 
+#if BEEPER_ENABLED == 1
+static void beeper_task()
+{
+    static int task_delay = 100;
+    static int step = 0;
+    static bool print_flag = true;
+
+    while (true) {
+        if(beeper_state != next_beeper_state) {
+            if(step == 0) {
+                printf("ALARM state: %d\n", beeper_state);
+                beeper_state = next_beeper_state;
+                step = 0;
+            }
+        }
+        switch(beeper_state) {
+            case ALARM_OFF:
+                if(print_flag){
+                    printf("ALARM: OFF\n");
+                    print_flag = false;
+                }
+                task_delay = 100;
+                break;
+            case ALARM_ARM:
+                print_flag = true;
+                switch(step) {
+                    case 0:
+                        printf("ALARM: ARM\n");
+                        gpio_put(beeper_pin, 1);
+                        task_delay = 300;
+                        step++;
+                    break;
+                    case 1:
+                        gpio_put(beeper_pin, 0);
+                        task_delay = 500;
+                        step++;
+                    break;
+                    case 2:
+                        gpio_put(beeper_pin, 1);
+                        task_delay = 1000;
+                        step++;
+                    break;
+                    case 3:
+                        gpio_put(beeper_pin, 0);
+                        task_delay = 100;
+                        step = 0;
+                        next_beeper_state = ALARM_OFF;
+                    break;
+                }
+                break;
+            case ALARM_DISARM:
+                print_flag = true;
+                switch(step) {
+                    case 0:
+                        printf("ALARM: DISARM\n");
+                        gpio_put(beeper_pin, 1);
+                        task_delay = 1500;
+                        step++;
+                    break;
+                    case 1:
+                        gpio_put(beeper_pin, 0);
+                        task_delay = 100;
+                        step = 0;
+                        next_beeper_state = ALARM_OFF;
+                    break;
+                }
+                break;
+            case ALARM_DETECTION:
+                print_flag = true;
+                switch(step) {
+                    case 0:
+                        printf("ALARM: DETECTION\n");
+                        gpio_put(beeper_pin, 1);
+                        task_delay = 300;
+                        step++;
+                    break;
+                    case 1:
+                        gpio_put(beeper_pin, 0);
+                        task_delay = 300;
+                        step = 0;
+                    break;
+                }
+                break;
+            case ALARM_RUN:
+                print_flag = true;
+                switch(step) {
+                    case 0:
+                        printf("ALARM: RUN\n");
+                        gpio_put(beeper_pin, 1);
+                        task_delay = 1000;
+                        step++;
+                    break;
+                    case 1:
+                        gpio_put(beeper_pin, 0);
+                        task_delay = 500;
+                        step = 0;
+                    break;
+                }
+                break;
+        }
+        vTaskDelay(pdMS_TO_TICKS(task_delay));
+    }
+}
+#endif
+
 static void on_tcp_connection(int con_id)
 {
     printf("--> TCP connected, connection id: %d\n", con_id);
@@ -136,11 +241,19 @@ static void on_tcp_connection(int con_id)
     server_connection = con_id;
 #if DHT_ENABLED == 1
     push_dht_all = true;
+    printf("--> start temp task.\n");
     xTaskCreate(temp_task, "TEMP_Task", 1024, NULL, TCP_TASK_PRIORITY+1, NULL);
 #endif
 #if CONT_ENABLED == 1
     push_con_all = true;
+    printf("--> start contactron task.\n");
     xTaskCreate(contactron_task, "CONT_Task", 1024, NULL, TCP_TASK_PRIORITY-1, NULL);
+#endif
+#if BEEPER_ENABLED == 1
+    beeper_state = ALARM_OFF;
+    next_beeper_state = ALARM_OFF;
+    printf("--> start beeper task.\n");
+    xTaskCreate(beeper_task, "BEEP_Task", 1024, NULL, TCP_TASK_PRIORITY+2, NULL);
 #endif
 }
 
@@ -175,6 +288,23 @@ static void on_tcp_msg_received(int con_id, char* msg)
         gpio_put(gate_pin, 1);
         sleep_ms(300);
         gpio_put(gate_pin, 0);
+    }
+#endif
+#if BEEPER_ENABLED == 1
+    else if (strcmp(msg, "alarm_off") == 0) {
+        next_beeper_state = ALARM_OFF;
+    }
+    else if (strcmp(msg, "alarm_arm") == 0) {
+        next_beeper_state = ALARM_ARM;
+    }
+    else if (strcmp(msg, "alarm_disarm") == 0) {
+        next_beeper_state = ALARM_DISARM;
+    }
+    else if (strcmp(msg, "alarm_detect") == 0) {
+        next_beeper_state = ALARM_DETECTION;
+    }
+    else if (strcmp(msg, "alarm_run") == 0) {
+        next_beeper_state = ALARM_RUN;
     }
 #endif
 #if HEAT_ENABLED == 1
@@ -236,6 +366,13 @@ int main(void)
     gpio_init(gate_pin);
     gpio_set_dir(gate_pin, GPIO_OUT);
     gpio_put(gate_pin, 0);
+#endif
+
+#if BEEPER_ENABLED == 1
+    // Init gate control pin
+    gpio_init(beeper_pin);
+    gpio_set_dir(beeper_pin, GPIO_OUT);
+    gpio_put(beeper_pin, 0);
 #endif
 
     sleep_ms(1000);
